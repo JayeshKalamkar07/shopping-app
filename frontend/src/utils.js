818e0c147da358b024048721bb9d48ce6e39af5e
Jayesh Kalamkar <86825998+JayeshKalamkar07@users.noreply.github.com>
2021-08-19 10:29:57 +0530

    Add files via upload

---
+++ frontend/src/utils.js
@@ -0,0 +1,46 @@
+export const prices = [{
+        name: 'Any',
+        min: 0,
+        max: 0,
+    },
+    {
+        name: `₹500 to ₹5000`,
+        min: 500,
+        max: 5000,
+    },
+    {
+        name: `₹5000 to ₹10000`,
+        min: 5000,
+        max: 10000,
+    },
+    {
+        name: `₹50000 to ₹150000`,
+        min: 50000,
+        max: 100000,
+    },
+    {
+        name: `₹150000 to ₹1000000`,
+        min: 100000,
+        max: 1000000,
+    },
+];
+export const ratings = [{
+        name: '4stars & up',
+        rating: 4,
+    },
+
+    {
+        name: '3stars & up',
+        rating: 3,
+    },
+
+    {
+        name: '2stars & up',
+        rating: 2,
+    },
+
+    {
+        name: '1stars & up',
+        rating: 1,
+    },
+];
